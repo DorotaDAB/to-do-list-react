5d493bbc3f0ca1b02fd791bb4c53c56becb3d2e1
DorotaDAB <dorota.dabrowska.kowalska@gmail.com>
2019-10-12 17:33:22 +0200

    Passing name to task component (TaskList hardcoded)

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -2,15 +2,23 @@ import React from 'react';
 import './Task.css';
 
 class Task extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			name: this.props.name,
+    }
+	}
+
 	render() {
-	return (
-		<div className="form-check">
-			<input className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
-			<label className="form-check-label">
-				task
-			</label>
-		</div>
-	)}
+		return (
+			<div className="form-check">
+				<input className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
+				<label className="form-check-label">
+					{this.state.name}
+				</label>
+			</div>
+		)}
 };
 
 export default Task;
--- src/Components/TasksForm/TasksForm.js
+++ src/Components/TasksForm/TasksForm.js
@@ -3,23 +3,34 @@ import './TasksForm.css';
 import Task from '../Task/Task';
 
 class TasksForm extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+      tasks: ["task1", "task2"],
+    }
+	}
+
 	render() {
-	return (
-		<>
-			<div className="input-group mb-3">
-				<input type="text" className="form-control" placeholder="Wprowadź zadanie" maxLength="100"/>
-				<div className="input-group-append">
-					<button className="btn btn btn-primary" type="button" id="button-addon2">+</button>
+		return (
+			<>
+				<div className="input-group mb-3">
+					<input type="text" className="form-control" placeholder="Wprowadź zadanie" maxLength="100"/>
+					<div className="input-group-append">
+						<button className="btn btn btn-primary" type="button" id="button-addon2">+</button>
+					</div>
 				</div>
-			</div>
-			<div className="card">
-				<ul className="list-group list-group-flush">
-						<li className="list-group-item"> <Task /> </li>
-						<li className="list-group-item"> <Task /> </li>
-				</ul>
-			</div>
-    </>
-	)}
+				<div className="card">
+					<ul className="list-group list-group-flush">
+						{this.state.tasks.map( (task, index) => {
+									return <li className="list-group-item" key={index}> <Task name={task}/> </li>
+								})
+						}
+					</ul>
+				</div>
+			</>
+		)
+	}
 };
 
 export default TasksForm;
