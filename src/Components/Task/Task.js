4e56433c56708518c03bceea03521e11605d97f5
DorotaDAB <dorota.dabrowska.kowalska@gmail.com>
2019-10-13 11:25:43 +0200

    Mark task as done/not done

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -7,16 +7,44 @@ class Task extends React.Component {
 
 		this.state = {
 			name: this.props.name,
-    }
+			isDone: false 
+		}
+		
+		this.toggleDoneStatus = this.toggleDoneStatus.bind(this);
+		this.displayLabel = this.displayLabel.bind(this);
+	}
+
+	toggleDoneStatus() {
+		this.setState({isDone: !this.state.isDone});
+	}
+
+	displayLabel() {
+		if (this.state.isDone) {
+			return (	
+				<label className="form-check-label" style={{color:"#00b300"}}>
+					{this.state.name}
+				</label>
+			)
+		} 
+		else {
+			return (
+				<label className="form-check-label">
+					{this.state.name}
+				</label>
+			)
+		}
 	}
 
 	render() {
 		return (
 			<div className="form-check">
-				<input className="form-check-input" type="checkbox" value="" />
-				<label className="form-check-label">
-					{this.state.name}
-				</label>
+				<input 
+					className="form-check-input" 
+					type="checkbox" 
+					defaultChecked={this.state.isDone}
+					onClick={this.toggleDoneStatus} 
+				/>
+				{this.displayLabel()}
 			</div>
 		)}
 };
